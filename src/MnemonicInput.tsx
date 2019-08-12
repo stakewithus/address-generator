f71d8e8ff84aba4e78b1f4e6e182614606f785dc
Simon Warta <simon@warta.it>
2019-08-12 12:25:02 +0200

    Process input if some whitespace included

--- src/MnemonicInput.tsx
+++ src/MnemonicInput.tsx
@@ -1,6 +1,6 @@
 import { EnglishMnemonic } from "@iov/crypto";
 import React from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { Typeahead, TypeaheadModel } from "react-bootstrap-typeahead";
 
 const wordlist = [...EnglishMnemonic.wordlist]; // create copy of correct type
 
@@ -13,7 +13,13 @@ interface MnemonicInputState {
   readonly words: readonly string[];
 }
 
+interface PrivateTypeahead<T extends TypeaheadModel> extends Typeahead<T> {
+  readonly _updateSelected: (selected: T[]) => void;
+}
+
 class MnemonicInput extends React.Component<MnemonicInputProps, MnemonicInputState> {
+  private typeahead: PrivateTypeahead<string> | null = null;
+
   public constructor(props: MnemonicInputProps) {
     super(props);
     this.state = {
@@ -28,10 +34,23 @@ class MnemonicInput extends React.Component<MnemonicInputProps, MnemonicInputSta
           id={this.props.id}
           multiple={true}
           bsSize="lg"
+          ref={ref => (this.typeahead = ref as PrivateTypeahead<string>)}
           onChange={selected => {
             console.log(selected);
             this.setState({ words: selected });
             this.props.onWordsChanged && this.props.onWordsChanged(selected);
+          }}
+          onInputChange={input => {
+            if (!this.typeahead) return;
+
+            // Process input if some whitespace included
+            if (input.search(/\s+/) !== -1) {
+              const oldWords = this.state.words;
+              const newWords = input.trim().split(/\s+/);
+
+              this.typeahead.setState({ text: "" });
+              this.typeahead._updateSelected([...oldWords, ...newWords]);
+            }
           }}
           selectHintOnEnter={true}
           options={wordlist}
