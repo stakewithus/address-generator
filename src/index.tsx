61b1831d851ac22c52e882a9045024c4d72702b2
Simon Warta <simon@warta.it>
2019-08-12 09:45:00 +0200

    Use hash router

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import "react-bootstrap-typeahead/css/Typeahead.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Redirect, Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/mainnet" component={() => <App network="mainnet" />} />
       <Route exact path="/testnet" component={() => <App network="testnet" />} />
       <Route component={() => <Redirect to="/mainnet" />} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root"),
 );
